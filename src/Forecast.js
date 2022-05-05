e936546a45e52dea1c42a108703c2f28d4f2f64e
Josh Bello <Jb230913@live.com>
2022-05-05 20:20:51 +0100

    modified useEffect() in forecast to decied to use geolocatin or not /  tryingto seperate today forecast with future

--- src/Forecast.js
+++ src/Forecast.js
@@ -2,35 +2,84 @@ import React,{useEffect,useState} from 'react'
 import { useLocation } from 'react-router-dom';
 import { ForecastList } from './components/ForecastList';
 import axios from 'axios';
+import { Row,Col } from 'react-bootstrap';
 const apiKey = process.env.REACT_APP_ACCESS_KEY;
-const Forecast = () => {
+function Forecast () {
+  var todayDate = new Date();
+  todayDate = todayDate.toISOString().split('T')[0];
+  const [currentLocationLat,setCurrentlocationLat] = useState('');
+  const [currentLocationLong,setCurrentlocationLong] = useState('');
+  const [currentLocForecast,setcurrentLocForecast] = useState([]);
+  const [todayForecast,settodayForecast] = useState([]);
   const location = useLocation();
-  const { forecastlong } = location.state
-  const { forecastlat } = location.state
+  
 
-  // console.log(forecastlong);
-  // console.log(forecastlat);
 
   useEffect(() => {
-    if (forecastlong !== null &&forecastlat !== null) {
+   if (!location) { //If user click button
+    const { forecastlong } = location.state
+    const { forecastlat } = location.state
       const fetchCurrentLocationForecast= async()=>{
         const getCurrentCity = await axios.get(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${forecastlat}&longitude=${forecastlong}&localityLanguage=en`)
         var searchCity = getCurrentCity.data.city;
         const result = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${searchCity}&appid=${apiKey}`)
-        console.log(result);
+        setcurrentLocForecast(result.data.list);
+        //console.log(result.data.list);
        }
 
        fetchCurrentLocationForecast();
-    } else {
+       setTodaysAndFutureForecast(currentLocForecast);
+    } else { // User must click allow for geolocation
+      navigator.geolocation.getCurrentPosition(function(position) {
 
+        setCurrentlocationLat(position.coords.latitude);
+        setCurrentlocationLong(position.coords.longitude)
+      })
+
+      const fetchCurrentLocationForecast= async()=>{
+        const getCurrentCity = await axios.get(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${currentLocationLat}&longitude=${currentLocationLong}&localityLanguage=en`)
+        var searchCity = getCurrentCity.data.city;
+        const result = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${searchCity}&appid=${apiKey}`)
+        setcurrentLocForecast(result.data.list);
+        //console.log(currentLocForecast);
+       }
+
+       fetchCurrentLocationForecast();
+       setTodaysAndFutureForecast(currentLocForecast);
     }
 
 
-  }, [])
-  
+  }, [location])
+  //console.log(currentLocForecast);
+  function setTodaysAndFutureForecast(data){
+    const TodaysForecast = [];
+    for (let index = 0; index < data.length; index++) {
+      // const element = array[index];
+      //console.log(data[index]);
+        let date = data[index].dt_txt;
+        date = date.split(' ');
+        date = date[0];
+      if (todayDate === date) {
+        TodaysForecast.push(data[index]);
+        data.splice(data.length, 1, data[index]);
+      } 
+
+    }
+    
+
+  }
+
+  //const TodaysForecast = '';
+  console.log(todayForecast);
+  console.log(currentLocForecast);
 
   return (
-    <div>Forecast</div>
+    <Row>
+    <Col md={12}>
+    <ForecastList/>
+    </Col>
+    </Row>
+  
   )
 }
 
--- src/components/ForecastList.js
+++ src/components/ForecastList.js
@@ -1,10 +1,7 @@
 import React,{useState} from 'react'
 
 export const ForecastList = () => {
-    
-    const [currentLocationWeather,setcurrentLocationWeather] = useState([]);
-    const [isloading,setisloading] = useState(true);
-    const [show,setShow] = useState(true);
+
   return (
     <div >ForecastList</div>
   )
